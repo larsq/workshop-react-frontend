af12ca85cd8bbc74a0d018070f78734ed30ac141
larsq <6453476+larsq@users.noreply.github.com>
2022-05-27 06:31:29 +0200

    Step 7b: React on selection

--- apps/moviedb/src/app/components/narrative-list-element.tsx
+++ apps/moviedb/src/app/components/narrative-list-element.tsx
@@ -1,10 +1,12 @@
 export interface NarrativeListElementProps {
   title: string,
-  isSelected: boolean
+  isSelected: boolean,
+  handleSelectionCallback: (val: string) => void
 }
 
 export function NarrativeListElement(props: NarrativeListElementProps) {
   const classes = `search-result__item ${props.isSelected ? 'search-result__item--selected' : ''}`
 
-  return <article className={classes}>{props.title}</article>
+  return <article className={classes}
+                  onClick={() => props.handleSelectionCallback(props.title)}>{props.title}</article>
 }
--- apps/moviedb/src/app/components/narrative-list.tsx
+++ apps/moviedb/src/app/components/narrative-list.tsx
@@ -1,6 +1,6 @@
 import '../styles/narrative-list.scss'
 import '../styles/support.scss'
-import {useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {NarrativeListElement} from "./narrative-list-element";
 import {NarrativeListResult} from "./narrative-list-result";
 
@@ -15,8 +15,34 @@ export interface NarrativeListStateProps {
 export function NarrativeList(props: NarrativeListProps) {
   const [state, setState] = useState<NarrativeListStateProps>({})
 
+  useEffect(() => {
+    if (props.titles.length === 0) {
+      setState({selectedTitle: undefined})
+      return;
+    }
+
+    if (props.titles.length === 1) {
+      setState({selectedTitle: props.titles[0]})
+      return;
+    }
+
+    if (state.selectedTitle !== undefined && !props.titles.includes(state.selectedTitle)) {
+      setState({selectedTitle: undefined})
+      return;
+    }
+  }, [props.titles, state.selectedTitle])
+
+  const handleSelection = useCallback((val: string) => {
+    if (props.titles.includes(val)) {
+      setState({selectedTitle: val})
+    } else {
+      setState({selectedTitle: undefined})
+    }
+  }, [props.titles])
+
   return <aside className="search-result">
-    {props.titles.map(title => <NarrativeListElement title={title} key={title} isSelected={title === state.selectedTitle}/>)}
+    {props.titles.map(title => <NarrativeListElement title={title} key={title} isSelected={title === state.selectedTitle}
+                                                     handleSelectionCallback={handleSelection}/>)}
     <div className="support__v-gap"></div>
     <NarrativeListResult matches={props.titles.length}/>
   </aside>
