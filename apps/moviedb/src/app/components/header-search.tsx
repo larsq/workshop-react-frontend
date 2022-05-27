3a09d6802843f2fae8d47843f6a37e84943bf7ba
larsq <6453476+larsq@users.noreply.github.com>
2022-05-27 09:37:34 +0200

    Step 7c: React on search

--- apps/moviedb/src/app/components/header-search.tsx
+++ apps/moviedb/src/app/components/header-search.tsx
@@ -1,11 +1,39 @@
 import '../styles/support.scss'
 import '../styles/header-search.scss'
+import {useRef} from "react";
 
 export function HeaderSearch() {
+  const searchbar = useRef<HTMLInputElement>(null)
+
+
+  function handleSearch(): void {
+    const ref: HTMLInputElement | null = searchbar.current
+
+    if (ref == null) {
+      throw Error('expects reference to be set')
+    }
+
+    const val = ref.value.toLowerCase()
+
+    if (val.length < 3) {
+      return;
+    }
+
+    console.log('"', val, '"')
+  }
+
   return <section className="navigation__searchbar">
-    <form>
+    <form onSubmit={(event) => {
+      handleSearch()
+      event.preventDefault()
+    }}>
       <div className="searchbar__component">
-        <input type="search" className="searchbar__field" name="search" placeholder="Search for title"></input>
+        <input type="text"
+               className="searchbar__field"
+               name="search"
+               placeholder="Search for title"
+               ref={searchbar}
+        ></input>
       </div>
     </form>
   </section>
